3dbf475204580cf646724c0c67844c4ae7466ab6
何炜平 <3091523763@qq.com>
2024-07-29 00:55:40 +0800

    注释了所有和Email实体相关的代码

--- service/src/main/java/org/netmen/service/impl/EmailServiceImpl.java
+++ service/src/main/java/org/netmen/service/impl/EmailServiceImpl.java
@@ -1,24 +1,24 @@
-package org.netmen.service.impl;
-
-import org.netmen.service.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
-import otg.netmen.common.dto.EmailProperties;
-import otg.netmen.common.utils.MailUtil;
-
-public class EmailServiceImpl implements EmailService {
-    @Autowired
-    private EmailProperties emailProperties;
-
-    /**
-     * @param to 接收人邮箱
-     * @param title 邮件标题
-     * @param content 邮件正文
-     * @return
-     */
-    @Override
-    public boolean send(String to, String title, String content) {
-        System.out.println(emailProperties);
-        boolean flag = MailUtil.sendMail(emailProperties, to, title, content);
-        return false;
-    }
-}
+//package org.netmen.service.impl;
+//
+//import org.netmen.service.EmailService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import otg.netmen.common.dto.EmailProperties;
+//import otg.netmen.common.utils.MailUtil;
+//
+//public class EmailServiceImpl implements EmailService {
+//    @Autowired
+//    private EmailProperties emailProperties;
+//
+//    /**
+//     * @param to 接收人邮箱
+//     * @param title 邮件标题
+//     * @param content 邮件正文
+//     * @return
+//     */
+//    @Override
+//    public boolean send(String to, String title, String content) {
+//        System.out.println(emailProperties);
+//        boolean flag = MailUtil.sendMail(emailProperties, to, title, content);
+//        return false;
+//    }
+//}
