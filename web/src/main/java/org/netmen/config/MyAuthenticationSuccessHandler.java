9de76ad45f546ec87db7c8bc4df3098108292612
YanG <xichenyuy@qq.com>
2024-08-06 11:04:44 +0800

    AuthenticationSuccessHandler

---
+++ web/src/main/java/org/netmen/config/MyAuthenticationSuccessHandler.java
@@ -0,0 +1,16 @@
+package org.netmen.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+    }
+}
