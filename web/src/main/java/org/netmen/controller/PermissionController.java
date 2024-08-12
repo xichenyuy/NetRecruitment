00b37a510192d1c7fd5e5ccdbfbe35a2b44b955a
xichenyuy <17880790228@163.com>
2024-08-12 14:02:58 +0800

    加入DTO

--- web/src/main/java/org/netmen/controller/PermissionController.java
+++ web/src/main/java/org/netmen/controller/PermissionController.java
@@ -3,7 +3,9 @@ package org.netmen.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.netmen.common.result.Result;
 import org.netmen.dao.po.Permission;
+import org.netmen.dto.PermissionDTO;
 import org.netmen.service.PermissionService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,9 +29,12 @@ public class PermissionController {
 
 
     @PostMapping
-    private Result add(@RequestBody Permission permission){
+    private Result add(@RequestBody PermissionDTO permissionDTO){
+        Permission permission = new Permission();
+        BeanUtils.copyProperties(permissionDTO, permission);
+        permissionService.save(permission);
         return Result.success();
     }
 
-    //检查Permission对象在swagger中有无参数提示
+    //检查Permission对象在swagger中有无参数提示 对象也会有参数提示
 }
