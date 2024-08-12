8c50be28d19a650f1dd5ef0e398a090bb0c81051
xichenyuy <17880790228@163.com>
2024-08-12 16:48:25 +0800

    permission的更新和逻辑删除

--- dao/src/main/java/org/netmen/dao/po/BaseEntity.java
+++ dao/src/main/java/org/netmen/dao/po/BaseEntity.java
@@ -6,6 +6,7 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
 public class BaseEntity {
     @TableId(type = IdType.AUTO)
     private Integer id;
--- web/src/main/java/org/netmen/controller/PermissionController.java
+++ web/src/main/java/org/netmen/controller/PermissionController.java
@@ -10,6 +10,7 @@ import org.netmen.dto.PermissionDTO;
 import org.netmen.service.PermissionService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,8 +23,8 @@ public class PermissionController {
     private PermissionService permissionService;
 
     @GetMapping
-    private Result list(Integer pageNum, Integer pageSize){
-        //传统返回
+    public Result list(Integer pageNum, Integer pageSize){
+        //返回
         //List<Permission> list = permissionService.list();
         //return Result.success().data(list);
         //分页返回
@@ -33,19 +34,34 @@ public class PermissionController {
         //List拷贝类的使用样例 暂时不需要封装PageVo
         //List<UserVo> userVos = BeanCopyUtils.copyBeanList(userPage.getRecords(), UserVo.class);
         //PageVo pageVo = new PageVo(userVos, userPage.getTotal());
+        //TODO
+        //增加筛选条件
     }
 
-
-
-
     @PostMapping
-    private Result add(@RequestBody PermissionDTO permissionDTO){
+    public Result add(@RequestBody PermissionDTO permissionDTO){
         Permission permission = new Permission();
         BeanUtils.copyProperties(permissionDTO, permission);
         //Permission permission1 = BeanCopyUtil.copyBean(permissionDTO, Permission.class);  //使用自定义封装的拷贝工具
         permissionService.save(permission);
         return Result.success();
+        //TODO
+        //校验name perms唯一性 或者在数据库中设置唯一索引
+        //插入后返回插入后数据库中的数据
     }
-    //TODO
-    //校验name perms唯一性
+
+    @PutMapping("/{id}")
+    public Result update(@PathVariable Integer id, @RequestBody PermissionDTO permissionDTO){
+        Permission permission = new Permission();
+        BeanUtils.copyProperties(permissionDTO, permission);
+        permission.setId(id);
+        permissionService.updateById(permission);
+        return Result.success();
+    }
+
+    @DeleteMapping("/{id}")
+    public Result delete(@PathVariable Integer id){
+        permissionService.removeById(id);
+        return Result.success();
+    }
 }
