4f4de2932710d927a63c726287bd88db9ffc7e5c
xichenyuy <17880790228@163.com>
2024-08-13 11:17:35 +0800

    role的增删查改

--- web/src/main/java/org/netmen/controller/RoleController.java
+++ web/src/main/java/org/netmen/controller/RoleController.java
@@ -1,6 +1,13 @@
 package org.netmen.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.netmen.common.result.Result;
+import org.netmen.dao.po.Role;
+import org.netmen.dto.RoleDTO;
+import org.netmen.service.RoleService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,27 +16,46 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "role")
 public class RoleController {
 
+    @Autowired
+    private RoleService roleService;
+
     @GetMapping
     @PreAuthorize("hasAnyAuthority('role:list')")
-    public String list() {
-        return "role list";
+    public Result<Object> list(Integer pageNum, Integer pageSize) {
+        Page<Role> page = new Page<>(pageNum, pageSize);
+        Page<Role> rolePage = roleService.page(page);
+        return Result.success().data(rolePage);
     }
 
     @PostMapping
     @PreAuthorize("hasAnyAuthority('role:add')")
-    public String add() {
-        return "新增成功";
+    public Result<Object> add(@RequestBody RoleDTO roleDTO) {
+        Role role = new Role();
+        BeanUtils.copyProperties(roleDTO, role);
+        roleService.save(role);
+        return Result.success();
     }
 
-    @PutMapping
+    @PutMapping("/{id}")
     @PreAuthorize("hasAnyAuthority('role:update')")
-    public String update() {
-        return "更新成功";
+    public Result<Object> update(Integer id, @RequestBody RoleDTO roleDTO) {
+        Role role = roleService.getById(id);
+        if(role == null){
+            return Result.error().message("ID不存在");
+        }
+        BeanUtils.copyProperties(roleDTO, role);
+        roleService.updateById(role);
+        return Result.success();
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     @PreAuthorize("hasAnyAuthority('role:delete')")
-    public String delete() {
-        return "删除成功";
+    public Result<Object> delete(Integer id) {
+        Role byId = roleService.getById(id);
+        if(byId == null){
+            return Result.error().message("ID不存在");
+        }
+        roleService.removeById(id);
+        return Result.success();
     }
 }
