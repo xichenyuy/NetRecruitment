e8e6606da216d01f3c46b6feebba52ead9cc72be
YanG <xichenyuy@qq.com>
2024-08-14 19:49:55 +0800

    配置前端专用的test5

--- web/src/test/java/org/netmen/JwtTest.java
+++ web/src/test/java/org/netmen/JwtTest.java
@@ -13,11 +13,11 @@ public class JwtTest {
         System.out.println(jwt);
     }
 
-    @Test
-    public void parseJwt() throws Exception {
-        Claims claims = JwtUtil.parseJwt("eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0NGFjZTY3ZDJmZjA0NzAzODYyYzk2YThiOGVlNDJhYiIsInN1YiI6IjEwMDg2IiwiaXNzIjoibmV0bWVuIiwiaWF0IjoxNzIzNjAyNjczLCJleHAiOjE3MjM2MDYyNzN9.MLM7VQmKEKi4ZdSbpB7kAuZgmeZW79hHsDYxl0Nk0wg");
-        System.out.println(claims);
-        System.out.println(claims.getId());
-    }
+    // @Test
+    // public void parseJwt() throws Exception {
+    //     Claims claims = JwtUtil.parseJwt("eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0NGFjZTY3ZDJmZjA0NzAzODYyYzk2YThiOGVlNDJhYiIsInN1YiI6IjEwMDg2IiwiaXNzIjoibmV0bWVuIiwiaWF0IjoxNzIzNjAyNjczLCJleHAiOjE3MjM2MDYyNzN9.MLM7VQmKEKi4ZdSbpB7kAuZgmeZW79hHsDYxl0Nk0wg");
+    //     System.out.println(claims);
+    //     System.out.println(claims.getId());
+    // }
 }
 
