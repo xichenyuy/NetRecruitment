c7fbc70d8247e57f91c36df0e21f6ff44a187a8c
LIn-ZQ-lin <649688031@qq.com>
2024-08-21 15:13:04 +0800

    Delete dao/src/main/java/org/netmen/dao/mapper/RoleMapper.java

--- dao/src/main/java/org/netmen/dao/mapper/RoleMapper.java
+++
@@ -1,8 +0,0 @@
-package org.netmen.dao.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.netmen.dao.po.Role;
-@Mapper
-public interface RoleMapper extends BaseMapper<Role> {
-}
