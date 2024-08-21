f777ae9f9ce9b32e213228289882444e5adb3f86
何炜平 <3091523763@qq.com>
2024-08-21 15:46:43 +0800

    删除其他分支内容1

--- service/src/main/java/org/netmen/service/PermissionService.java
+++
@@ -1,7 +0,0 @@
-package org.netmen.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import org.netmen.dao.po.Permission;
-
-public interface PermissionService extends IService<Permission> {
-}
--- service/src/main/java/org/netmen/service/RoleService.java
+++
@@ -1,7 +0,0 @@
-package org.netmen.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import org.netmen.dao.po.Role;
-
-public interface RoleService extends IService<Role> {
-}
