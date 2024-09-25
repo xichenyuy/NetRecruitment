41c2145393f9c68de91b2b31683c1e291b57eadf
龚海琳 <1695824489@qq.com>
2024-09-25 11:22:49 +0800

    重新编写了interview_record的类，请先不着急合并到main分支中

--- service/src/main/java/org/netmen/service/InterviewRecordService.java
+++ service/src/main/java/org/netmen/service/InterviewRecordService.java
@@ -2,6 +2,8 @@ package org.netmen.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import org.netmen.dao.po.InterviewRecord;
+import org.netmen.dao.po.InterviewStatus;
+import org.netmen.dao.po.Student;
 
 import java.util.List;
 import java.util.Map;
@@ -9,4 +11,21 @@ import java.util.Map;
 public interface InterviewRecordService extends IService<InterviewRecord> {
 
     List<Map<String, Object>> getInterviewRecordList(Integer departmentId, Integer pageNum, Integer pageSize);
+
+    Map<String,Object> convertToMap(InterviewRecord interviewRecord);
+
+    List<InterviewRecord> getStudentInterviewRecordList(Integer studentId);
+
+    //初始化一个填了报名表的学生，这个接口应该在student处调用,用来插入第一条数据
+    boolean initStudentInterviewRecord(Integer studentId, Integer currentDepartmentId);
+
+    boolean adjustStudentInterviewRecord(Integer studentId, Integer adjustDepartmentId);
+
+    Integer interviewPass(Integer studentId, Integer curDepartmentId);
+
+    boolean interviewFailed(Integer studentId, Integer firstDepartmentId, Integer secondDepartmentId);
+
+    boolean falseTouchRejection(Integer studentId,Integer firstDepartmentId, Integer secondDepartmentId);
+
+    boolean initialize(Integer studentId);
 }
--- web/src/main/java/org/netmen/controller/InterviewRecordController.java
+++ web/src/main/java/org/netmen/controller/InterviewRecordController.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import io.swagger.v3.oas.annotations.Operation;
 import org.netmen.common.result.Result;
 import org.netmen.dao.po.InterviewRecord;
+import org.netmen.dao.po.InterviewStatus;
 import org.netmen.service.InterviewRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,61 +18,24 @@ public class InterviewRecordController {
     @Autowired
     private InterviewRecordService interviewRecordService;
 
-    @PostMapping
-    @Operation(summary = "增加面试记录",description = "传入要添加面试者的部门id、要被加入面试的学生id、所加入的面试的级别（也就是第几轮面试）")
-    public Result add(Integer departmentId,Integer studentId,Integer priority){
-        LambdaQueryWrapper<InterviewRecord> wrapper = new LambdaQueryWrapper<>();
-        wrapper.eq(InterviewRecord::getDepartmentId,departmentId).eq(InterviewRecord::getStudentId,studentId);
-        InterviewRecord interviewRecord = interviewRecordService.getOne(wrapper);
+//    @PostMapping
+//    @Operation(summary = "增加面试记录",description = "传入要添加面试者的部门id、要被加入面试的学生id、所加入的面试的级别（也就是第几轮面试）")
+//    public Result add(Integer departmentId,Integer studentId,Integer priority){
+//        return interviewRecordService.add(departmentId,studentId,priority);
+//    }
 
-        //检验一下存不存在
-        if(interviewRecord == null){
+//    @PostMapping("/{id}")
+//    @Operation(summary = "更新此学生的面试轮次",description = "传入此学生的面试记录id,与要更换到的轮次")
+//    public Result updateInterview(@PathVariable Integer id,Integer priority){
+//        return interviewRecordService.updateInterview(id,priority);
+//
+//    }
 
-            InterviewRecord newOne = new InterviewRecord();
-            newOne.setDepartmentId(departmentId);
-            newOne.setStudentId(studentId);
-            newOne.setFailed(false);
-            newOne.setPriority(priority);
-            newOne.setDeleted(false);
-            interviewRecordService.save(newOne);
-        }else if(interviewRecord.getPriority() == priority){
-            return Result.error().message("此记录已经存在！");
-        }else{
-            return Result.error().message("与已经存在的记录有冲突");
-        }
-
-        return Result.success().message("记录增加成功");
-    }
-
-    @PostMapping("/{id}")
-    @Operation(summary = "更新此学生的面试轮次",description = "传入此学生的面试记录id,与要更换到的轮次")
-    public Result updateInterview(@PathVariable Integer id,Integer priority){
-        InterviewRecord interviewRecord = interviewRecordService.getById(id);
-        if(interviewRecord == null){
-            return Result.error().message("不存在此学生的面试记录");
-        }
-        interviewRecord.setPriority(priority);
-        boolean res = interviewRecordService.updateById(interviewRecord);
-        if(!res){
-            return Result.error().message("更新失败!");
-        }
-        return Result.success().message("更新成功!");
-    }
-
-    @PostMapping("/eliminate")
-    @Operation(summary = "淘汰此学生",description = "传入此学生的面试记录id")
-    public Result eliminate(Integer id){
-        InterviewRecord interviewRecord = interviewRecordService.getById(id);
-        if(interviewRecord == null){
-            return Result.error().message("不存在此学生的面试记录");
-        }
-        interviewRecord.setFailed(true);
-        boolean res = interviewRecordService.updateById(interviewRecord);
-        if(!res){
-            return Result.error().message("更新失败!");
-        }
-        return Result.success().message("更新成功!");
-    }
+//    @PostMapping("/eliminate/{id}")
+//    @Operation(summary = "淘汰此学生",description = "传入此学生的面试记录id")
+//    public Result eliminate(@PathVariable Integer id){
+//        return interviewRecordService.eliminate(id);
+//    }
 
     @GetMapping
     @Operation(summary = "部门面试记录",description = "传入想要获取信息的部门id，获取这个部门有关的所有面试记录")
@@ -83,17 +47,85 @@ public class InterviewRecordController {
         return Result.success().message("成功找到此部门的记录").data(list);
     }
 
-    @DeleteMapping("/{id}")
-    @Operation(summary = "删除此学生面试记录",description = "传入想要删除的面试记录id")
-    public Result deleted(@PathVariable Integer id){
-        InterviewRecord interviewRecord = interviewRecordService.getById(id);
-        if(interviewRecord == null){
-            return Result.error().message("此条记录不存在！");
+    @GetMapping("/{studentId}")
+    @Operation(summary = "学生面试记录",description = "传入学生学号id，获取这个学生的所有面试记录")
+    public Result getStudentInterviewRecordList(@PathVariable Integer studentId){
+        List<InterviewRecord> list = interviewRecordService.getStudentInterviewRecordList(studentId);
+        List<Map<String,Object>> res = list.stream().map(interviewRecord -> {
+            try{
+                return interviewRecordService.convertToMap(interviewRecord);
+            }catch (Exception e){
+                throw new RuntimeException("InterviewRecord转换成Map出错:",e);
+            }
+        }).toList();
+        if(list == null || list.isEmpty()){
+            return Result.error().message("没有找到此学生的记录");
         }
-        boolean res = interviewRecordService.removeById(id);
-        if(!res){
-            return Result.error().message("删除失败！");
-        }
-        return Result.success().message("删除成功！");
+        return Result.success().message("成功找到此学生的记录").data(res);
     }
+
+//    @DeleteMapping("/{id}")
+//    @Operation(summary = "删除此学生面试记录",description = "传入想要删除的面试记录id")
+//    public Result deleted(@PathVariable Integer id){
+//        return interviewRecordService.deleted(id);
+//    }
+
+    /**
+     * 以下接口均为测试方法，可以不用看
+     */
+//    @PostMapping("/{studentId}")
+//    @Operation(summary = "插入第一条数据")
+//    public Result initStudentInterviewRecord(@PathVariable Integer studentId,Integer currentDepartmentId){
+//        if(interviewRecordService.initStudentInterviewRecord(studentId,currentDepartmentId))
+//            return Result.success();
+//        return Result.error();
+//    }
+//
+//    @PostMapping("/update/{studentId}")
+//    @Operation(summary = "更新面试者的记录")
+//    public Result interviewPass(@PathVariable Integer studentId, Integer curDepartmentId){
+//        int res = interviewRecordService.interviewPass(studentId,curDepartmentId);
+//        if(res == 1){
+//            return Result.success().message("通过所有面试，待录取");
+//        }else if(res == 0){
+//            return Result.success().message("继续面试");
+//        }
+//        return Result.error().message("更新失败！");
+//    }
+//
+//    @PostMapping("/back/{studentId}")
+//    @Operation(summary = "回退")
+//    public Result falseTouchRejection(@PathVariable Integer studentId,Integer firstDepartmentId, Integer secondDepartmentId){
+//        boolean res = interviewRecordService.falseTouchRejection(studentId,firstDepartmentId,secondDepartmentId);
+//        if(res){
+//            return Result.success();
+//        }
+//        return Result.error();
+//    }
+//
+//    @PostMapping("/fail/{studentId}")
+//    @Operation(summary = "淘汰学生")
+//    public Result interviewFailed(@PathVariable Integer studentId, Integer firstDepartmentId, Integer secondDepartmentId){
+//        if(interviewRecordService.interviewFailed(studentId,firstDepartmentId,secondDepartmentId)){
+//            return Result.success();
+//        }
+//        return Result.error();
+//    }
+//
+//    @PostMapping("/initialize/{studentId}")
+//    @Operation(summary = "初始化")
+//    public Result initialize(@PathVariable Integer studentId){
+//        if(interviewRecordService.initialize(studentId))
+//            return Result.success();
+//        return Result.error();
+//    }
+//
+//   @PostMapping("/adjust/{studentId}")
+//   @Operation(summary = "调剂")
+//   public Result adjustStudentInterviewRecord(@PathVariable Integer studentId,Integer adjustDepartmentId){
+//        if(interviewRecordService.adjustStudentInterviewRecord(studentId,adjustDepartmentId))
+//            return Result.success();
+//        return Result.error();
+//   }
+
 }
