029d37f1033a50df4a60fdb61f5128fbbe29bee4
龚海琳 <1695824489@qq.com>
2024-10-04 18:15:45 +0800

    修改了回退方法，纠正了第一志愿和第二志愿重合时的记录插入错误

--- web/src/main/java/org/netmen/controller/InterviewRecordController.java
+++ web/src/main/java/org/netmen/controller/InterviewRecordController.java
@@ -75,7 +75,7 @@ public class InterviewRecordController {
     /**
      * 以下接口均为测试方法，可以不用看
      */
-    @PostMapping("/{studentId}")
+    @PostMapping("/priority/{studentId}")
     @Operation(summary = "看看能不能查找到priority")
     public Result getCurpriority(@PathVariable Integer studentId){
         int res = interviewRecordService.getCurPriority(studentId);
@@ -100,45 +100,45 @@ public class InterviewRecordController {
         }
         return Result.success().message("删除成功");
     }
-//    @PostMapping("/{studentId}")
-//    @Operation(summary = "插入第一条数据")
-//    public Result initStudentInterviewRecord(@PathVariable Integer studentId,Integer currentDepartmentId){
-//        if(interviewRecordService.initStudentInterviewRecord(studentId,currentDepartmentId))
-//            return Result.success();
-//        return Result.error();
-//    }
-//
-//    @PostMapping("/update/{studentId}")
-//    @Operation(summary = "更新面试者的记录")
-//    public Result interviewPass(@PathVariable Integer studentId, Integer curDepartmentId){
-//        int res = interviewRecordService.interviewPass(studentId,curDepartmentId);
-//        if(res == 1){
-//            return Result.success().message("通过所有面试，待录取");
-//        }else if(res == 0){
-//            return Result.success().message("继续面试");
-//        }
-//        return Result.error().message("更新失败！");
-//    }
-//
-//    @PostMapping("/back/{studentId}")
-//    @Operation(summary = "回退")
-//    public Result falseTouchRejection(@PathVariable Integer studentId,Integer firstDepartmentId, Integer secondDepartmentId){
-//        boolean res = interviewRecordService.falseTouchRejection(studentId,firstDepartmentId,secondDepartmentId);
-//        if(res){
-//            return Result.success();
-//        }
-//        return Result.error();
-//    }
-//
-//    @PostMapping("/fail/{studentId}")
-//    @Operation(summary = "淘汰学生")
-//    public Result interviewFailed(@PathVariable Integer studentId, Integer firstDepartmentId, Integer secondDepartmentId){
-//        if(interviewRecordService.interviewFailed(studentId,firstDepartmentId,secondDepartmentId)){
-//            return Result.success();
-//        }
-//        return Result.error();
-//    }
-//
+    @PostMapping("/{studentId}")
+    @Operation(summary = "插入第一条数据")
+    public Result initStudentInterviewRecord(@PathVariable Integer studentId,Integer currentDepartmentId){
+        if(interviewRecordService.initStudentInterviewRecord(studentId,currentDepartmentId))
+            return Result.success();
+        return Result.error();
+    }
+
+    @PostMapping("/update/{studentId}")
+    @Operation(summary = "更新面试者的记录")
+    public Result interviewPass(@PathVariable Integer studentId, Integer curDepartmentId){
+        int res = interviewRecordService.interviewPass(studentId,curDepartmentId);
+        if(res == 1){
+            return Result.success().message("通过所有面试，待录取");
+        }else if(res == 0){
+            return Result.success().message("继续面试");
+        }
+        return Result.error().message("更新失败！");
+    }
+
+    @PostMapping("/back/{studentId}")
+    @Operation(summary = "回退")
+    public Result falseTouchRejection(@PathVariable Integer studentId,Integer firstDepartmentId, Integer secondDepartmentId){
+        boolean res = interviewRecordService.falseTouchRejection(studentId,firstDepartmentId,secondDepartmentId);
+        if(res){
+            return Result.success();
+        }
+        return Result.error();
+    }
+
+    @PostMapping("/fail/{studentId}")
+    @Operation(summary = "淘汰学生")
+    public Result interviewFailed(@PathVariable Integer studentId, Integer firstDepartmentId, Integer secondDepartmentId){
+        if(interviewRecordService.interviewFailed(studentId,firstDepartmentId,secondDepartmentId)){
+            return Result.success();
+        }
+        return Result.error();
+    }
+
     @PostMapping("/initialize/{studentId}")
     @Operation(summary = "初始化")
     public Result initialize(@PathVariable Integer studentId){
@@ -146,13 +146,13 @@ public class InterviewRecordController {
             return Result.success();
         return Result.error();
     }
-//
-//   @PostMapping("/adjust/{studentId}")
-//   @Operation(summary = "调剂")
-//   public Result adjustStudentInterviewRecord(@PathVariable Integer studentId,Integer adjustDepartmentId){
-//        if(interviewRecordService.adjustStudentInterviewRecord(studentId,adjustDepartmentId))
-//            return Result.success();
-//        return Result.error();
-//   }
+
+   @PostMapping("/adjust/{studentId}")
+   @Operation(summary = "调剂")
+   public Result adjustStudentInterviewRecord(@PathVariable Integer studentId,Integer adjustDepartmentId){
+        if(interviewRecordService.adjustStudentInterviewRecord(studentId,adjustDepartmentId))
+            return Result.success();
+        return Result.error();
+   }
 
 }
